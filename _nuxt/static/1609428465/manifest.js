61caa485f65d0dd3bd59a263ca9d905fb0f26a3f
gcman105 <gcman105@users.noreply.github.com>
2021-01-01 11:59:23 +0000

    deploy: b68cec4d35d9e11bc85f175d117c4fadb9d6ccb5

--- _nuxt/static/1609428465/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact","\u002Fposts","\u002Fposts\u002Fgreat-article","\u002Fposts\u002Fsecond-great-article"]})
---
+++ _nuxt/static/1609502362/about/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/about", (function(a){return {data:[{page:{slug:"about",description:"gcman105 online, work & play",title:"gcman105=Gary C.",author:"Gary Cheeseman",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002Fabout",extension:".md",createdAt:a,updatedAt:a}}],fetch:[],mutations:void 0}}("2021-01-01T11:58:14.830Z")));
